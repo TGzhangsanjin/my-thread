637d5984f49a7ae444828206f28d9ecb8202ba20
zhangxin <jizhibao>
2021-11-02 14:39:44 +0800

    失败了，，没看出线程的效果更好

--- src/main/java/fiber/HelloFiber.java
+++ src/main/java/fiber/HelloFiber.java
@@ -1,11 +1,80 @@
 package fiber;
 
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.strands.SuspendableRunnable;
+
 /**
  *
+ * 失败了，，没看出线程的效果更好
  * @Author 张三金
  * @Date 2021/11/2 0002 13:57
  * @Company jzb
  * @Version 1.0.0
  */
 public class HelloFiber {
+
+    public static void main(String[] args) throws Exception{
+
+        int size = 5_000_000;
+        noThread(size);
+        myThread(size);
+        userFiber(size);
+    }
+
+    static void userFiber (int size) throws Exception{
+        long start = System.currentTimeMillis();
+        Fiber<Void>[] fibers = new Fiber[size];
+        for (int i = 0; i < fibers.length; i++) {
+            fibers[i] = new Fiber<Void>(new SuspendableRunnable() {
+                @Override
+                public void run() throws SuspendExecution, InterruptedException {
+                    calculate();
+                }
+            });
+        }
+        for (int i = 0; i < fibers.length; i++) {
+            fibers[i].start();
+        }
+        for (int i = 0; i < fibers.length; i++) {
+            fibers[i].join();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("开启 " + size + "个纤程，计算耗时： " + (end - start) + "ms");
+    }
+
+    static void noThread (int size){
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            calculate();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("单个进程，计算耗时： " + (end - start) + "ms");
+    }
+
+    static void myThread (int size) throws Exception{
+        long start = System.currentTimeMillis();
+        Runnable runnable = HelloFiber::calculate;
+        Thread[] threads = new Thread[size];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(runnable);
+        }
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("开启 " + size + "个线程，计算耗时： " + (end - start) + "ms");
+    }
+
+    /**
+     * 啥也不干就是用来耗时间用的
+     */
+    static void calculate () {
+        int result = 0;
+        for (int i= 0; i < 10000; i ++) {
+            for (int j = 0; j < 200; j++) {
+
+            }
+        }
+    }
 }
