d724714810709827bf2fed365a650717b21b36de
zhangxin <jizhibao>
2022-01-22 15:18:19 +0800

    弱引用  --  WeakReference

--- src/main/java/juc/reference/Code03_WeakReference.java
+++ src/main/java/juc/reference/Code03_WeakReference.java
@@ -4,6 +4,10 @@ import java.lang.ref.WeakReference;
 
 /**
  * 弱引用  --  遭遇到 GC 就会被回收
+ * 一个弱引用的对象如果同时被一个强引用所指向，当这个强引用消失的时候，这个弱应用就应该被gc回收，
+ * 弱引用 在 ThreadLocal 的 Entry 对象是这样的
+ *
+ * @TODO ThreadLocal 中的一张图需要画一下
  * @Author 张三金
  * @Date 2022/1/22 0022 15:02
  * @Company jzb
